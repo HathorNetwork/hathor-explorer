5da91fcb8307fd7877f90005a1fd04a163b9f12c
Pedro Ferreira <phsf.pedro@gmail.com>
2018-09-05 18:08:38 +0200

    feat(admin): creating navigation and basic wallet page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import Admin from './components/Admin';
+import Wallet from './components/Wallet';
+import Navigation from './components/Navigation';
 
 class Root extends React.Component {
   render() {
     return (
     <Router>
       <Switch>
-        <Route exact path="" component={Admin} />
+        <NavigationRoute exact path="/wallet" component={Wallet} />
+        <NavigationRoute exact path="" component={Admin} />
       </Switch>
     </Router>
     )
   }
 }
+
+const NavigationRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+      <div><Navigation {...props}/><Component {...props} /></div>
+  )} />
+)
 
 export default Root;
---
+++ src/components/Wallet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Wallet extends React.Component {
+  render() {
+
+    return (
+      <div>
+        Wallet
+      </div>
+    );
+  }
+}
+
+export default Wallet;
