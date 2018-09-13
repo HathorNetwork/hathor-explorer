289af18771c2e97c5c5ec3ea1ef03480e1d4a741
Pedro Ferreira <phsf.pedro@gmail.com>
2018-09-13 23:34:55 +0000

    feat: websocket in the wallet

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import walletApi from '../api/wallet';
 
 const helpers = {
   showAlert(id, duration) {
@@ -8,6 +9,19 @@ const helpers = {
       el.removeClass('show');
     }, duration);
   },
+
+  checkWalletLock(unlock, lock) {
+    walletApi.checkLock().then((data) => {
+      if (!data.is_locked) {
+        unlock();
+      } else {
+        lock();
+      }
+    }, (e) => {
+      // Error in request
+      console.log(e);
+    });
+  }
 }
 
 export default helpers;
