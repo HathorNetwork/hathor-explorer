7c34f65687b9e251703c3d8c781a3ca36d6fd3d9
Pedro Ferreira <phsf.pedro@gmail.com>
2018-10-30 03:13:45 +0000

    fix: updating wallet balance in both mobile and desktop version

--- src/components/WalletBalance.js
+++ src/components/WalletBalance.js
@@ -1,47 +1,5 @@
 import React from 'react';
-import walletApi from '../api/wallet';
 
-
-class WalletBalance extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      balance: null
-    }
-
-    this.updateBalance = this.updateBalance.bind(this);
-  }
-
-  componentDidMount() {
-    walletApi.getBalance().then((data) => {
-      this.updateBalance(data.balance);
-      this.props.loaded();
-    }, (e) => {
-      // Error in request
-      console.log(e);
-    });
-  }
-
-  updateBalance(balance) {
-    this.setState({balance})
-  }
-
-  render() {
-    const renderBalance = () => {
-      return (
-        <div>
-          <p><strong>Balance:</strong> {this.state.balance} hathor{this.state.balance === 1 ? '' : 's'}</p>
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        {renderBalance()}
-      </div>
-    );
-  }
-}
+const WalletBalance = ({balance}) => <div><p><strong>Balance:</strong> {balance} hathor{balance === 1 ? '' : 's'}</p></div>;
 
 export default WalletBalance;
