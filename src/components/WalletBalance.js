6ab859682e7b046cd57366c93b089dd253ec1d88
Pedro Ferreira <phsf.pedro@gmail.com>
2018-12-04 09:47:02 +0000

    feat(wallet): showing wallet balance depending if we have locked balance or not

--- src/components/WalletBalance.js
+++ src/components/WalletBalance.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import helpers from '../utils/helpers';
 
-const WalletBalance = ({balance}) => <div><p><strong>Balance:</strong> {balance !== undefined ? helpers.prettyValue(balance) : ''} hathor{balance === 1 ? '' : 's'}</p></div>;
+const WalletBalance = ({balance}) => {
+  const renderBalance = () => {
+    return (
+      <div>
+        <p><strong>Total:</strong> {helpers.prettyValue(balance.available + balance.locked)} hathor{balance === 1 ? '' : 's'}</p>
+        <p><strong>Available:</strong> {helpers.prettyValue(balance.available)} hathor{balance === 1 ? '' : 's'}</p>
+        <p><strong>Locked:</strong> {helpers.prettyValue(balance.locked)} hathor{balance === 1 ? '' : 's'}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {balance && renderBalance()}
+    </div>
+  );
+};
 
 export default WalletBalance;
