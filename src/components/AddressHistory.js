f97e41a18bd04cd0c80af36aaf193ea02500cd9c
Pedro Ferreira <pedro@hathor.network>
2020-03-24 00:36:17 -0300

    refactor: moving logic to screen file and components now just render the data

--- src/components/AddressHistory.js
+++ src/components/AddressHistory.js
@@ -6,184 +6,14 @@
  */
 
 import React from 'react';
-import ReactLoading from 'react-loading';
 import { Link } from 'react-router-dom';
-import { TX_COUNT } from '../constants';
-import { isEqual } from 'lodash';
 import dateFormatter from '../utils/date';
-import helpers from '../utils/helpers';
-import WebSocketHandler from '../WebSocketHandler';
-import colors from '../index.scss';
 import hathorLib from '@hathor/wallet-lib';
 import PropTypes from 'prop-types';
 import PaginationURL from '../utils/pagination';
 
 
 class AddressHistory extends React.Component {
-  /**
-   * transactions {Array} List of transactions on the list
-   * loading {boolean} If is waiting response of data request
-   * firstHash {String} First hash of the list, used in pagination
-   * lastHash {String} Last hash of the list, used in pagination
-   * hasAfter {boolean} If has more elements after the list page
-   * hasBefore {boolean} If has more elements before the list page
-   * queryParams {Object} Object with URL parameters data
-   * numberOfTransactions {Number} Total number of transactions of the list
-   */
-  state = {
-    transactions: [],
-    loading: true,
-    firstHash: null,
-    lastHash: null,
-    hasAfter: false,
-    hasBefore: false,
-    queryParams: null,
-    numberOfTransactions: 0,
-  }
-
-  componentDidMount() {
-    this.setState({ queryParams: this.props.pagination.obtainQueryParams() }, () => {
-      if (this.state.queryParams.token !== null) {
-        this.getData(this.state.queryParams);
-      }
-    });
-
-    WebSocketHandler.on('network', this.handleWebsocket);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.address !== prevProps.address) {
-      // This case will be handled by the other component, which will add the token in the URL
-      // and then call this method again later
-      return;
-    }
-
-    const queryParams = this.props.pagination.obtainQueryParams();
-
-    // Do we have new URL params?
-    if (!isEqual(this.state.queryParams, queryParams)) {
-      // Fetch new data, unless query params were cleared and we were already in the most recent page
-      if (queryParams.hash || this.state.hasBefore) {
-        this.getData(queryParams);
-      }
-
-      if (queryParams.token !== this.state.queryParams.token) {
-        // User selected a new token, so we must go to the first page (clear queryParams)
-        this.props.pagination.clearOptionalQueryParams();
-        this.getData(this.props.pagination.obtainQueryParams());
-      }
-    }
-  }
-
-  componentWillUnmount() {
-    WebSocketHandler.removeListener('network', this.handleWebsocket);
-  }
-
-  /**
-   * Called when 'network' ws message arrives
-   * If it's a new tx message update data, in case is necessary
-   *
-   * @param {Object} wsData Data from websocket
-   */
-  handleWebsocket = (wsData) => {
-    if (wsData.type === 'network:new_tx_accepted') {
-      this.updateListWs(wsData);
-    }
-  }
-
-  /**
-   * Update the list with a new tx arrived in a ws message
-   * Checks if the list is on the first page, so it can have a new element
-   * Also validates if this tx is compatible with the list
-   *
-   * @param {Object} tx Data of a newly arrived tx
-   */
-  updateListWs = (tx) => {
-    // We only add new tx/blocks if it's the first page
-    if (!this.state.hasBefore) {
-      if (this.props.shouldUpdate(tx, true)) {
-        let transactions = this.state.transactions;
-        let hasAfter = (this.state.hasAfter || (transactions.length === TX_COUNT && !this.state.hasAfter))
-        transactions = helpers.updateListWs(transactions, tx, TX_COUNT);
-
-        let firstHash = transactions[0].tx_id;
-        let lastHash = transactions[transactions.length-1].tx_id;
-
-        const newNumberOfTransactions = this.state.numberOfTransactions + 1;
-
-        // Finally we update the state again
-        this.setState({ transactions, hasAfter, firstHash, lastHash, numberOfTransactions: newNumberOfTransactions });
-        if (this.props.addressSummaryRef.current) {
-          this.props.addressSummaryRef.current.updateNumberOfTransactions(newNumberOfTransactions);
-        }
-      }
-    }
-  }
-
-  /**
-   * Update component state when new list data arrives
-   *
-   * @param {Object} data Response data from the server
-   * @param {Object} queryParams URL parameters
-   */
-  handleDataFetched = (data, queryParams) => {
-    // Handle differently if is the first GET response we receive
-    // page indicates if was clicked 'previous' or 'next'
-    // Set first and last hash of the transactions
-    let firstHash = null;
-    let lastHash = null;
-    if (data.transactions.length) {
-      firstHash = data.transactions[0].tx_id;
-      lastHash = data.transactions[data.transactions.length-1].tx_id;
-    }
-
-    let hasAfter;
-    let hasBefore;
-    if (queryParams.page === 'previous') {
-      hasAfter = true;
-      hasBefore = data.has_more;
-      if (!hasBefore) {
-        // Went back to most recent page: clear URL params
-        this.props.pagination.clearOptionalQueryParams();
-      }
-    } else if (queryParams.page === 'next') {
-      hasBefore = true;
-      hasAfter = data.has_more;
-    } else {
-      // First load without parameters
-      hasBefore = false;
-      hasAfter = data.has_more;
-    }
-
-    this.setState({
-      transactions: data.transactions,
-      loading: false,
-      firstHash,
-      lastHash,
-      hasAfter,
-      hasBefore,
-      queryParams,
-      numberOfTransactions: data.total,
-    });
-
-    if (this.props.addressSummaryRef.current) {
-      this.props.addressSummaryRef.current.updateNumberOfTransactions(data.total);
-    }
-  }
-
-  /**
-   * Update transactions data state after requesting data from the server
-   *
-   * @param {Object} queryParams URL parameters
-   */
-  getData = (queryParams) => {
-    hathorLib.walletApi.getSearchAddress(this.props.address, TX_COUNT, queryParams.hash, queryParams.page, queryParams.token, (response) => {
-      if (response.success) {
-        this.handleDataFetched(response, queryParams);
-      }
-    });
-  }
-
   /**
    * We get the final balance for the search address and selected token
    *
@@ -192,7 +22,7 @@ class AddressHistory extends React.Component {
    * @return {Number} Final balance value (can be negative value, in case we spent more than received for the search address)
    */
   calculateAddressBalance = (tx) => {
-    const token = this.props.pagination.obtainQueryParams().token;
+    const token = this.props.selectedToken;
     let value = 0;
 
     for (let txin of tx.inputs) {
@@ -238,22 +68,30 @@ class AddressHistory extends React.Component {
   }
 
   render() {
-    if (this.state.transactions.length === 0) {
+    if (this.props.transactions.length === 0) {
       return <p>This address does not have any transactions yet.</p>
     }
 
+    const getFirstHash = () => {
+      return this.props.transactions[0].tx_id;
+    }
+
+    const getLastHash = () => {
+      return this.props.transactions[this.props.transactions.length-1].tx_id;
+    }
+
     const loadPagination = () => {
-      if (this.state.transactions.length === 0) {
+      if (this.props.transactions.length === 0) {
         return null;
       } else {
         return (
           <nav aria-label="Tx pagination" className="d-flex justify-content-center">
             <ul className="pagination">
-              <li ref="txPrevious" className={(!this.state.hasBefore || this.state.transactions.length === 0) ? "page-item mr-3 disabled" : "page-item mr-3"}>
-                <Link className="page-link" to={this.props.pagination.paginationUrl({hash: this.state.firstHash, page: 'previous'})}>Previous</Link>
+              <li ref="txPrevious" className={(!this.props.hasBefore || this.props.transactions.length === 0) ? "page-item mr-3 disabled" : "page-item mr-3"}>
+                <Link className="page-link" to={this.props.pagination.paginationUrl({hash: getFirstHash(), page: 'previous'})}>Previous</Link>
               </li>
-              <li ref="txNext" className={(!this.state.hasAfter || this.state.transactions.length === 0) ? "page-item disabled" : "page-item"}>
-                <Link className="page-link" to={this.props.pagination.paginationUrl({hash: this.state.lastHash, page: 'next'})}>Next</Link>
+              <li ref="txNext" className={(!this.props.hasAfter || this.props.transactions.length === 0) ? "page-item disabled" : "page-item"}>
+                <Link className="page-link" to={this.props.pagination.paginationUrl({hash: getLastHash(), page: 'next'})}>Next</Link>
               </li>
             </ul>
           </nav>
@@ -285,7 +123,7 @@ class AddressHistory extends React.Component {
     }
 
     const loadTableBody = () => {
-      return this.state.transactions.map((tx, idx) => {
+      return this.props.transactions.map((tx, idx) => {
         const value = this.calculateAddressBalance(tx);
         let statusElement = '';
         let trClass = '';
@@ -326,7 +164,7 @@ class AddressHistory extends React.Component {
 
     return (
       <div className="w-100">
-        {this.state.loading ? <ReactLoading type='spin' color={colors.purpleHathor} delay={500} /> : loadTable()}
+        {loadTable()}
         {loadPagination()}
       </div>
     );
@@ -335,17 +173,21 @@ class AddressHistory extends React.Component {
 
 /*
  * address: Address to show summary
- * shouldUpdate: Function that receives a tx data and returns if summary should be updated
  * onRowClicked: Function executed when user clicks on the table row (receives tx_id)
  * pagination: Instance of pagination class that handles the URL parameters
- * updateNumberOfTransactions: Function executed when the total number of transactions on the list changes
+ * selectedToken: UID of the selected token to show history
+ * transactions: Array of transactions to show in the history
+ * hasAfter: If has a page after to fetch new history
+ * hasBefore: If has a page before to fetch history
  */
 AddressHistory.propTypes = {
   address: PropTypes.string.isRequired,
-  shouldUpdate: PropTypes.func.isRequired,
   onRowClicked: PropTypes.func.isRequired,
   pagination: PropTypes.instanceOf(PaginationURL).isRequired,
-  updateNumberOfTransactions: PropTypes.func.isRequired,
+  selectedToken: PropTypes.string.isRequired,
+  transactions: PropTypes.array.isRequired,
+  hasAfter: PropTypes.bool.isRequired,
+  hasBefore: PropTypes.bool.isRequired,
 };
 
 
--- src/components/AddressSummary.js
+++ src/components/AddressSummary.js
@@ -6,140 +6,23 @@
  */
 
 import React from 'react';
-import ReactLoading from 'react-loading';
-import { Link } from 'react-router-dom';
-import {TX_COUNT} from '../constants';
-import TxRow from './TxRow';
-import helpers from '../utils/helpers';
-import WebSocketHandler from '../WebSocketHandler';
-import colors from '../index.scss';
 import hathorLib from '@hathor/wallet-lib';
 import PropTypes from 'prop-types';
-import PaginationURL from '../utils/pagination';
 
 
 class AddressSummary extends React.Component {
-  /**
-   * balance {Object} Object with balance of each token of this address indexed by tokenUid {uid1: {'received', 'spent'}}
-   * loading {boolean} If is waiting server response
-   * errorMessage {String} Message to be shown in case of an error
-   * selectedToken {String} UID of the selected token when address has many
-   * numberOfTransactions {Number} Total number of transactions of the list
-   */
-  state = {
-    balance: {},
-    loading: true,
-    errorMessage: '',
-    selectedToken: '',
-    numberOfTransactions: 0,
-  }
-
-  componentDidMount() {
-    const queryParams = this.props.pagination.obtainQueryParams();
-    if (queryParams.token !== null) {
-      // User already have a token selected on the URL
-      this.setState({ selectedToken: queryParams.token }, () => {
-        this.getData();
-      });
-    } else {
-      // Will get data and select the default token
-      this.getData();
-    }
-
-    WebSocketHandler.on('network', this.handleWebsocket);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.address !== prevProps.address) {
-      // Address changed, must update data
-      this.props.pagination.clearOptionalQueryParams();
-      this.getData();
-    }
-  }
-
-  componentWillUnmount() {
-    WebSocketHandler.removeListener('network', this.handleWebsocket);
-  }
-
-  /**
-   * Called when 'network' ws message arrives
-   * If it's a new tx message update data, in case is necessary
-   *
-   * @param {Object} wsData Data from websocket
-   */
-  handleWebsocket = (wsData) => {
-    if (wsData.type === 'network:new_tx_accepted') {
-      if (this.props.shouldUpdate(wsData, false)) {
-        this.getData();
-      }
-    }
-  }
-
-  /**
-   * Request data from server and update state balance
-   */
-  getData() {
-    hathorLib.walletApi.getAddressBalance(this.props.address, (response) => {
-      if (response.success) {
-        let selectedToken = '';
-        if (this.state.selectedToken && this.state.selectedToken in response.tokens_data) {
-          // If user had selected a token already, should continue the same
-          selectedToken = this.state.selectedToken;
-        } else {
-          const hathorUID = hathorLib.constants.HATHOR_TOKEN_CONFIG.uid
-          if (hathorUID in response.tokens_data) {
-            // If HTR is in the token list of this address, it's the default selection
-            selectedToken = hathorUID;
-          } else {
-            // Otherwise we get the first element
-            const keys = Object.keys(response.tokens_data);
-            selectedToken = response.tokens_data[keys[0]];
-          }
-        }
-
-        // Update token in the URL
-        this.updateTokenURL(selectedToken);
-
-        this.setState({
-          balance: response.tokens_data,
-          loading: false,
-          selectedToken,
-        });
-      } else {
-        this.setState({
-          loading: false,
-          errorMessage: response.message,
-        });
-      }
-    });
-  }
-
   /**
    * Called when selected token is changed
    *
    * @param {Object} e Event emitted when select is changed
    */
   selectChanged = (e) => {
-    this.setState({ selectedToken: e.target.value });
-    this.updateTokenURL(e.target.value);
-  }
-
-  updateTokenURL = (token) => {
-    const queryParams = this.props.pagination.obtainQueryParams();
-    queryParams.token = token;
-    const newURL = this.props.pagination.paginationUrl(queryParams);
-    this.props.pushNewURL(newURL);
-  }
-
-  updateNumberOfTransactions = (numberOfTransactions) => {
-    this.setState({ numberOfTransactions });
+    this.props.tokenSelectChanged(e.target.value);
   }
 
   render() {
-    if (Object.keys(this.state.balance).length === 0) {
-      if (this.state.loading) {
-        return null;
-      }
+    if (Object.keys(this.props.balance).length === 0) {
+      return null;
     }
 
     const loadMainInfo = () => {
@@ -147,19 +30,19 @@ class AddressSummary extends React.Component {
         <div className="card text-white bg-dark mb-3">
           <div className="card-body">
             Address: {this.props.address}<br />
-            Number of tokens: {Object.keys(this.state.balance).length}
+            Number of tokens: {Object.keys(this.props.balance).length}
           </div>
         </div>
       );
     }
 
     const loadBalanceInfo = () => {
-      const balance = this.state.balance[this.state.selectedToken];
+      const balance = this.props.balance[this.props.selectedToken];
       return (
         <div className="card bg-light mb-3">
           <div className="card-body">
             Token: {renderTokenData()}<br />
-            Number of transactions: {this.state.numberOfTransactions}<br />
+            Number of transactions: {this.props.numberOfTransactions}<br />
             Total received: {hathorLib.helpers.prettyValue(balance.received)}<br />
             Total spent: {hathorLib.helpers.prettyValue(balance.spent)}<br />
             <strong>Final balance: </strong>{hathorLib.helpers.prettyValue(balance.received - balance.spent)}
@@ -169,12 +52,12 @@ class AddressSummary extends React.Component {
     }
 
     const renderTokenData = () => {
-      if (Object.keys(this.state.balance).length === 1) {
-        const balance = this.state.balance[this.state.selectedToken];
+      if (Object.keys(this.props.balance).length === 1) {
+        const balance = this.props.balance[this.props.selectedToken];
         return <span>{balance.name} ({balance.symbol})</span>
       } else {
         return (
-          <select value={this.state.selectedToken} onChange={this.selectChanged}>
+          <select value={this.props.selectedToken} onChange={this.selectChanged}>
             {renderTokenOptions()}
           </select>
         );
@@ -182,8 +65,8 @@ class AddressSummary extends React.Component {
     }
 
     const renderTokenOptions = () => {
-      return Object.keys(this.state.balance).map((uid) => {
-        const tokenData = this.state.balance[uid];
+      return Object.keys(this.props.balance).map((uid) => {
+        const tokenData = this.props.balance[uid];
         return <option value={uid} key={uid}>{tokenData.name} ({tokenData.symbol})</option>;
       });
     }
@@ -199,8 +82,7 @@ class AddressSummary extends React.Component {
 
     return (
       <div className="w-100">
-        {this.state.loading ? <ReactLoading type='spin' color={colors.purpleHathor} delay={500} /> : loadSummary()}
-        <p className="text-danger mt-3">{this.state.errorMessage}</p>
+        {loadSummary()}
       </div>
     );
   }
@@ -208,13 +90,17 @@ class AddressSummary extends React.Component {
 
 /*
  * address: Address to show summary
- * shouldUpdate: Function that receives a tx data and returns if summary should be updated
- * pagination: Instance of pagination class that handles the URL parameters
+ * balance: Object with balance data of each token for the selected address {'uid': {name, symbol, received, spent}}
+ * selectedToken: UID of the selected token to show history
+ * numberOfTransactions: Number of transactions for the selected token and address
+ * tokenSelectChanged: Callback to be executed when user changes token
  */
 AddressSummary.propTypes = {
   address: PropTypes.string.isRequired,
-  shouldUpdate: PropTypes.func.isRequired,
-  pagination: PropTypes.instanceOf(PaginationURL).isRequired,
+  balance: PropTypes.object.isRequired,
+  selectedToken: PropTypes.string.isRequired,
+  numberOfTransactions: PropTypes.number.isRequired,
+  tokenSelectChanged: PropTypes.func.isRequired,
 };
 
 export default AddressSummary;
