b60447c9065d65c96fc0addbcf9850772f8794b1
Pedro Ferreira <pedro@hathor.network>
2020-09-03 23:34:03 -0300

    feat: using a custom axios instance to increase the request timeout

---
+++ src/api/customAxiosInstance.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import hathorLib from '@hathor/wallet-lib';
+
+/**
+ * Create axios instance settings base URL and content type  
+ * Besides that, it captures error to show modal error and save in Redux
+ *
+ * @module Axios
+ */
+
+/**
+ * Create an axios instance to be used when sending requests
+ *
+ * @param {callback} resolve Callback to be stored and used in case of a retry after a fail
+ * @param {number} timeout Timeout in milliseconds for the request
+ */
+const createRequestInstance = (resolve, timeout) => {
+  const instance = hathorLib.axios.defaultCreateRequestInstance(resolve, 30000);
+  return instance;
+}
+
+export default createRequestInstance;
