b0031a7ac1c8a922ffeff98551552948f643dbb3
Pedro Ferreira <pedro@hathor.network>
2020-09-10 11:18:03 -0300

    feat: stop realtime update address balance (#37)
    * fix: prevent duplicate address search request
    
    * feat: using a custom axios instance to increase the request timeout
    
    * feat: stop realtime address balance update and show a warning saying that has received more txs
    
    * chore: improving custom axios instance comments
    
    * feat: adding refresh page link

---
+++ src/api/customAxiosInstance.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import hathorLib from '@hathor/wallet-lib';
+
+/**
+ * Create axios instance to be used in lib HTTP requests
+ *
+ * @module Axios
+ */
+
+/**
+ * Create an axios instance to be used when sending requests
+ *
+ * @param {callback} resolve Callback to be stored and used in case of a retry after a fail
+ * @param {number} timeout Timeout in milliseconds for the request
+ */
+const createRequestInstance = (resolve, timeout) => {
+  // Will override lib axios instance increasing the default request timeout
+  const instance = hathorLib.axios.defaultCreateRequestInstance(resolve, 30000);
+  return instance;
+}
+
+export default createRequestInstance;
