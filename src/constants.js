3694b3402a26ed9c1c204fbeb1abf078bbcb4acc
Giovane Costa <gigio.coder@gmail.com>
2021-05-19 12:04:16 -0300

    feat(tx): adds helper function to get token list info

--- src/constants.js
+++ src/constants.js
@@ -26,21 +26,25 @@ export const DASHBOARD_TX_COUNT = 6;
 
 export const MAINNET_GENESIS_BLOCK = [
   '000006cb93385b8b87a545a1cbb6197e6caff600c12cc12fc54250d39c8088fc'
-]
+];
 
 export const MAINNET_GENESIS_TX = [
   '0002d4d2a15def7604688e1878ab681142a7b155cbe52a6b4e031250ae96db0a',
   '0002ad8d1519daaddc8e1a37b14aac0b045129c01832281fb1c02d873c7abbf9'
-]
+];
 
 export const TESTNET_GENESIS_BLOCK = [
   '0000033139d08176d1051fb3a272c3610457f0c7f686afbe0afe3d37f966db85',
-]
+];
 
 export const TESTNET_GENESIS_TX = [
   '00e161a6b0bee1781ea9300680913fb76fd0fac4acab527cd9626cc1514abdc9',
   '00975897028ceb037307327c953f5e7ad4d3f42402d71bd3d11ecb63ac39f01a',
-]
+];
+
+export const MAINNET_NFT_TOKEN_LIST_URL = ''; // TODO add json url
+
+export const TESTNET_NFT_TOKEN_LIST_URL = ''; // TODO add json url
 
 export const DECIMAL_PLACES = 2;
 
