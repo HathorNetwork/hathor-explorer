bdd8e64a2d8bb75314ace2c418a642ffbeb06970
Giovane Costa <gigio.coder@gmail.com>
2021-06-25 17:01:28 -0300

    refact: change message / remove unused code
    Signed-off-by: Giovane Costa <gigio.coder@gmail.com>

--- src/components/GDPRConsent.js
+++ src/components/GDPRConsent.js
@@ -8,36 +8,27 @@
 import React, { useEffect } from 'react';
 
 import { GTM_ID } from '../constants';
-import CookieConsent, { getCookieConsentValue }  from "react-cookie-consent";
+import CookieConsent from "react-cookie-consent";
 import TagManager from 'react-gtm-module';
 
 
 const GDPRConsent = () => {
-  
-  const onAccept = () => {
-    // Nothing yet
-  };
-  
+
   useEffect(() => {
     // Just to ensure that when we initialize GTM, the message on this component will be shown
     TagManager.initialize({ gtmId: GTM_ID });
-
-    const isConsent = getCookieConsentValue();
-    if (isConsent === "true") {
-      onAccept();
-    }
+    // To inicialize something the user already consented, we can use getCookieConsentValue method and test the returned value ('true' or 'false') here
   }, []);
 
   return (
     <CookieConsent
-      onAccept={onAccept}
       disableButtonStyles={true}
       buttonText="Got it!"
       buttonWrapperClasses="mx-auto"
       buttonClasses="btn btn-hathor m-3"
     >
-      We use cookies to improve your user experience. By continuing onto our website, you agree with our
-      <a href="https://hathor.network/terms-and-conditions/" target="_blank" rel="noopener noreferrer" > Terms and Conditions </a>.
+      This website uses cookies to ensure you get the best experience on our website.
+      <a href="https://hathor.network/terms-and-conditions/" target="_blank" rel="noopener noreferrer" > Learn more</a>.
     </CookieConsent>
   )
 }
