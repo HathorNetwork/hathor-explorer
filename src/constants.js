052e438556eff97b0dd3f721bbbc54ec909dfdac
Giovane Costa <gigio.coder@gmail.com>
2021-06-26 00:13:13 -0300

    chore: bump to v0.7.7

--- src/constants.js
+++ src/constants.js
@@ -45,7 +45,7 @@ export const TESTNET_GENESIS_TX = [
 
 export const DECIMAL_PLACES = 2;
 
-export const VERSION = '0.7.6';
+export const VERSION = '0.7.7';
 
 export const MIN_API_VERSION = '0.33.0';
 
