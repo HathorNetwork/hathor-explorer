318ed66418ecd0a18d54664d5e6679d3efd9ea92
Giovane Costa <gigio.coder@gmail.com>
2021-07-12 16:25:21 -0300

    feat: show tx metadata

--- src/components/TxData.js
+++ src/components/TxData.js
@@ -18,6 +18,8 @@ import dateFormatter from '../utils/date';
 import txApi from '../api/txApi';
 import { BASE_URL, HATHOR_TOKEN_INDEX, HATHOR_TOKEN_CONFIG } from '../constants';
 import hathorLib from '@hathor/wallet-lib';
+import TxAlerts from './tx/TxAlerts';
+import TxMarkers from './tx/TxMarkers';
 
 
 /**
@@ -444,12 +446,6 @@ class TxData extends React.Component {
       );
     }
 
-    const renderGenesisMark = (hash) => {
-      if (helpers.isGenesisBlock(hash) || helpers.isGenesisTx(hash)) {
-        return <span className="text-info">[GENESIS]</span>
-      }
-    }
-
     const loadTxData = () => {
       return (
         <div className="tx-data-wrapper">
@@ -457,7 +453,7 @@ class TxData extends React.Component {
           <div><label>{hathorLib.helpers.isBlock(this.props.transaction) ? 'Block' : 'Transaction'} ID:</label> {this.props.transaction.hash}</div>
           <div className="d-flex flex-column flex-lg-row align-items-start mt-3 mb-3">
             <div className="d-flex flex-column align-items-start common-div bordered-wrapper mr-lg-3 w-100">
-              <div><label>Type:</label> {hathorLib.helpers.getTxType(this.props.transaction)} {renderGenesisMark(this.props.transaction.hash)}</div>
+              <div><label>Type:</label> {hathorLib.helpers.getTxType(this.props.transaction)} <TxMarkers tx={this.props.transaction} /></div>
               <div><label>Time:</label> {dateFormatter.parseTimestamp(this.props.transaction.timestamp)}</div>
               <div><label>Nonce:</label> {this.props.transaction.nonce}</div>
               <div><label>Weight:</label> {helpers.roundFloat(this.props.transaction.weight)}</div>
@@ -520,6 +516,7 @@ class TxData extends React.Component {
 
     return (
       <div>
+        <TxAlerts tx={this.props.transaction} />
         {loadTxData()}
         <HathorAlert ref="alertCopied" text="Copied to clipboard!" type="success" />
       </div>
