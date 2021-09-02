4165d3b1db54b014a145ac9c79c83da9fc6f642e
Pedro Ferreira <pedro@hathor.network>
2021-09-02 12:03:17 -0300

    refactor: pdf type now is checked by type and not file extension

--- src/components/TokenNFTPreview.js
+++ src/components/TokenNFTPreview.js
@@ -49,27 +49,25 @@ const TokenNFTPreview = (props) => {
   let media;
 
   if (nftType === NFT_MEDIA_TYPES.image) {
-    if (ext === 'pdf') {
-      // Toolbar to prevent showing download/print icons
-      const data = `${token.meta.nft.file}#toolbar=0`;
-      media = <object data={data} width="100%" height="100%" type="application/pdf" alt="NFT Preview" />;
-    } else {
-      media = <img src={token.meta.nft.file} width="100%" height="100%" alt="NFT Preview" />;
-    }
-  } else if(nftType === NFT_MEDIA_TYPES.video && fileType) {
+    media = <img src={token.meta.nft.file} width="100%" height="100%" alt="NFT Preview" />;
+  } else if (nftType === NFT_MEDIA_TYPES.video && fileType) {
     media = (
       <video controls controlsList="nodownload noremoteplayback" disablePictureInPicture onPlay={onPlayMedia}>
         <source src={token.meta.nft.file} type={fileType} />
         Your browser does not support html video tag.
       </video>
     )
-  } else if(nftType === NFT_MEDIA_TYPES.audio && fileType) {
+  } else if (nftType === NFT_MEDIA_TYPES.audio && fileType) {
     media = (
       <audio controls controlsList="nodownload" onPlay={onPlayMedia} >
         <source src={token.meta.nft.file} type={fileType} />
         Your browser does not support the audio element.
       </audio>
     )
+  } else if (nftType === NFT_MEDIA_TYPES.pdf) {
+    // Toolbar to prevent showing download/print icons
+    const data = `${token.meta.nft.file}#toolbar=0`;
+    media = <object data={data} width="100%" height="100%" type="application/pdf" alt="NFT Preview" />;
   } else {
     media = <p> Preview Unavailable </p>
   }
