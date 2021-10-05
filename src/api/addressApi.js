2ce6f6aef7e5be54688ae91549bdbf8170d73d80
André Carneiro <andreluizmrcarneiro@gmail.com>
2021-10-05 16:17:36 -0300

    feat: call address balance and search from the explorer-service

---
+++ src/api/addressApi.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { requestExplorerServiceV1 } from './axiosInstance';
+
+const addressApi = {
+  getBalance(address) {
+    return requestExplorerServiceV1().get(`node_api/address_balance`, {params: {address}}).then((res) => {
+      if (res && id in res.data) {
+        return res.data
+      }
+    }).catch((error) => {
+      // something wrong with request
+    });
+  },
+
+  search(address, count, hash, page, token) {
+    /*
+     address: address to search
+     count: int -> how many objects we want
+     hash (optional): str -> hash reference for the pagination (works together with 'page' parameter)
+     page (optional): 'previous' or 'next' -> if 'previous', we get the objects before the hash reference
+                                   if 'next', we get the objects after the hash reference
+     token (optional): str -> only fetch txs related to this token uid
+    */
+    
+    const data = {address, count};
+    if (hash) {
+        data['hash'] = hash;
+        data['page'] = page;
+    }
+    if (token) {
+        data['token'] = token;
+    }
+
+    return requestExplorerServiceV1().get(`node_api/address_search`, {params: data}).then((res) => {
+      if (res && id in res.data) {
+        return res.data
+      }
+    }).catch((error) => {
+      // something wrong with request
+    });
+  },
+};
+
+export default addressApi;
--- src/screens/AddressDetail.js
+++ src/screens/AddressDetail.js
@@ -17,6 +17,7 @@ import { TX_COUNT } from '../constants';
 import { isEqual } from 'lodash';
 import helpers from '../utils/helpers';
 import metadataApi from '../api/metadataApi';
+import addressApi from '../api/addressApi';
 
 
 class AddressDetail extends React.Component {
@@ -178,7 +179,7 @@ class AddressDetail extends React.Component {
    * @param {Object} queryParams URL parameters
    */
   getHistoryData = (queryParams) => {
-    hathorLib.walletApi.getSearchAddress(this.state.address, TX_COUNT, queryParams.hash, queryParams.page, queryParams.token, (response) => {
+    addressApi.search(this.state.address, TX_COUNT, queryParams.hash, queryParams.page, queryParams.token, (response) => {
       if (response.success) {
         this.handleFetchedData(response, queryParams);
       }
@@ -228,7 +229,7 @@ class AddressDetail extends React.Component {
    * Request data from server and update state balance
    */
   getSummaryData = () => {
-    hathorLib.walletApi.getAddressBalance(this.state.address, (response) => {
+    addressApi.getBalance(this.state.address, (response) => {
       if (response.success) {
         let selectedToken = '';
         if (this.state.selectedToken && this.state.selectedToken in response.tokens_data) {
