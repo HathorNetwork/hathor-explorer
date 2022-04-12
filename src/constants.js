93e355d84d47323041a44810e7ea1205b25ddc76
Luis Helder <luislhl@gmail.com>
2022-04-12 13:25:35 -0300

    bump: v0.10.7 (#181)

--- src/constants.js
+++ src/constants.js
@@ -45,7 +45,7 @@ export const TESTNET_GENESIS_TX = [
 
 export const DECIMAL_PLACES = 2;
 
-export const VERSION = '0.10.6';
+export const VERSION = '0.10.7';
 
 export const MIN_API_VERSION = '0.33.0';
 
