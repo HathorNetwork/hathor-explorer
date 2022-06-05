9930d858ebaaf77805636b98b1694fda4d405447
André Abadesso <andre.abadesso@gmail.com>
2022-06-05 13:28:35 -0300

    fix: typo in navigation item name

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -74,7 +74,7 @@ class Navigation extends React.Component {
                   Tokens
                 </span>
                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <ConditionalNavigation to="/tokens" label="Tokens list" featureToggle={`${UNLEASH_TOKENS_BASE_FEATURE_FLAG}.rollout`} />
+                  <ConditionalNavigation to="/tokens" label="Token list" featureToggle={`${UNLEASH_TOKENS_BASE_FEATURE_FLAG}.rollout`} />
                   <ConditionalNavigation to="/token_balances" label="Token balances" featureToggle={`${UNLEASH_TOKEN_BALANCES_FEATURE_FLAG}.rollout`} />
                 </div>
               </li>
