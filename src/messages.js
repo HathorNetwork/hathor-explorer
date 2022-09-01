ab35abb84911702f81e46cd0461b9554f221eaa3
Pedro Ferreira <pedro@hathor.network>
2022-09-01 21:36:50 +0100

    feat: add new token/tx ban messages (#226)

--- src/messages.js
+++ src/messages.js
@@ -1,2 +1,2 @@
-export const tokenBannedMessage = ''; // TODO: this message is being disscussed here: https://github.com/HathorNetwork/hathor-explorer/issues/92
-export const txTokenBannedMessage = ''; // TODO: this message is being disscussed here: https://github.com/HathorNetwork/hathor-explorer/issues/92
+export const tokenBannedMessage = 'This token has been linked to suspicious activity by the community. Please exercise caution when interacting with this token.'; // TODO: this message is being disscussed here: https://github.com/HathorNetwork/hathor-explorer/issues/92
+export const txTokenBannedMessage = 'One of the tokens of this transaction has been linked to suspicious activity by the community. Please exercise caution when interacting with this token.'; // TODO: this message is being disscussed here: https://github.com/HathorNetwork/hathor-explorer/issues/92
