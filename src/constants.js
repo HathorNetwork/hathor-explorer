c656bdcfe55f7b2725ba4bdc74b45ddf354bf0ea
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-08-04 12:19:05 -0300

    chore: bump version v0.16.0

--- src/constants.js
+++ src/constants.js
@@ -52,7 +52,7 @@ export const TESTNET_GENESIS_TX = [
 
 export const DECIMAL_PLACES = 2;
 
-export const VERSION = '0.15.1';
+export const VERSION = '0.16.0';
 
 export const MIN_API_VERSION = '0.33.0';
 
