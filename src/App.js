f99d33be11ce88d7958411d1d8af871ad79cb553
Pedro Ferreira <pedro@hathor.network>
2024-01-23 19:13:04 -0300

    feat: add nano contract detail route to app js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import Dag from './screens/Dag';
 import Dashboard from './screens/Dashboard';
 import VersionError from './screens/VersionError';
 import WebSocketHandler from './WebSocketHandler';
+import NanoContractDetail from './screens/NanoContractDetail';
 import { apiLoadErrorUpdate, dashboardUpdate, isVersionAllowedUpdate } from "./actions/index";
 import { connect } from "react-redux";
 import versionApi from './api/version';
@@ -117,6 +118,7 @@ class Root extends React.Component {
               <NavigationRoute exact path="/statistics" component={Dashboard} />
               <NavigationRoute exact path="/token_detail/:tokenUID" component={TokenDetail} />
               <NavigationRoute exact path="/address/:address" component={AddressDetail} />
+              <NavigationRoute exact path="/nano_contract/detail/:nc_id" component={NanoContractDetail} />
               <NavigationRoute exact path="" component={DashboardTx} />
             </Switch>
           </Router>
