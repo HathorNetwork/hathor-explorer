e9915447bba6890a29cf5a6d89be99e6d521438a
Pedro Ferreira <pedro@hathor.network>
2024-04-12 17:37:55 -0300

    feat: add support for nano contract details (#253)

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ import Dag from './screens/Dag';
 import Dashboard from './screens/Dashboard';
 import VersionError from './screens/VersionError';
 import WebSocketHandler from './WebSocketHandler';
+import NanoContractDetail from './screens/nano/NanoContractDetail';
+import BlueprintDetail from './screens/nano/BlueprintDetail';
 import { apiLoadErrorUpdate, dashboardUpdate, isVersionAllowedUpdate } from "./actions/index";
 import { connect } from "react-redux";
 import versionApi from './api/version';
@@ -59,7 +61,11 @@ class Root extends React.Component {
     this.props.apiLoadErrorUpdate({apiLoadError: false});
 
     versionApi.getVersion().then((data) => {
-      hathorLibConfig.setNetwork(data.network.split('-')[0]);
+      let network = data.network;
+      if (data.network.includes('testnet')) {
+        network = 'testnet';
+      }
+      hathorLibConfig.setNetwork(network);
       this.props.isVersionAllowedUpdate({allowed: helpers.isVersionAllowed(data.version)});
     }, (e) => {
       // Error in request
@@ -117,6 +123,8 @@ class Root extends React.Component {
               <NavigationRoute exact path="/statistics" component={Dashboard} />
               <NavigationRoute exact path="/token_detail/:tokenUID" component={TokenDetail} />
               <NavigationRoute exact path="/address/:address" component={AddressDetail} />
+              <NavigationRoute exact path="/nano_contract/detail/:nc_id" component={NanoContractDetail} />
+              <NavigationRoute exact path="/blueprint/detail/:blueprint_id" component={BlueprintDetail} />
               <NavigationRoute exact path="" component={DashboardTx} />
             </Switch>
           </Router>
