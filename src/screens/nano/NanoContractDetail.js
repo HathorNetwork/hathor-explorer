552db1d0afcd6c2d46ab9a0ce26e10ffcf1a919e
Pedro Ferreira <pedro@hathor.network>
2024-08-06 18:40:49 -0300

    feat: format amount/timestamp nano state attributes

--- src/screens/nano/NanoContractDetail.js
+++ src/screens/nano/NanoContractDetail.js
@@ -6,6 +6,7 @@
  */
 
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Loading from '../../components/Loading';
 import TxRow from '../../components/tx/TxRow';
 import hathorLib from '@hathor/wallet-lib';
@@ -21,6 +22,10 @@ import { Link } from 'react-router-dom'
  */
 function NanoContractDetail(props) {
   const ncId = props.match.params.nc_id;
+
+  const { decimalPlaces } = useSelector((state) => {
+    return { decimalPlaces: state.serverInfo.decimal_places }
+  });
 
   // ncState {Object | null} Nano contract state
   const [ncState, setNcState] = useState(null);
@@ -168,7 +173,28 @@ function NanoContractDetail(props) {
     // In this case, we will show only the attribute type
     // In the future, we plan to have a query feature, so the user can
     // query these attributes until they get the value they need
-    return 'value' in data ? data.value : blueprintInformation.attributes[name];
+    if (!('value' in data)) {
+      // If the value is a dict, we show only the type for now
+      return blueprintInformation.attributes[name];
+    }
+
+    if (data.value == null) {
+      // If value is null or undefined, we show empty string
+      return null;
+    }
+
+    // Get type of value but removing possible optional mark (?) to format the value correctly
+    const type = blueprintInformation.attributes[name].replace('?', '');
+
+    if (type === 'Timestamp') {
+      return hathorLib.dateUtils.parseTimestamp(data.value);
+    }
+
+    if (type === 'Amount') {
+      return hathorLib.numberUtils.prettyValue(data.value, decimalPlaces);
+    }
+
+    return data.value;
   }
 
   const renderAttributes = () => {
