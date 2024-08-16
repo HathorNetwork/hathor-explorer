95890cd938c638761924bcf633a530643667afc6
tuliomir <tulio.mir@gmail.com>
2024-08-16 16:20:37 -0300

    refactor: DashboardTx to functional component

--- src/screens/DashboardTx.js
+++ src/screens/DashboardTx.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import TxRow from '../components/tx/TxRow';
 import WebSocketHandler from '../WebSocketHandler';
 import { DASHBOARD_TX_COUNT, DASHBOARD_BLOCKS_COUNT } from '../constants';
@@ -17,133 +17,128 @@ import helpers from '../utils/helpers';
  *
  * @memberof Screens
  */
-class DashboardTx extends React.Component {
-  /**
-   * transactions {Array} Array of transactions to show in the dashboard
-   * blocks {Array} Array of blocks to show in the dashboard
-   */
-  state = { transactions: [], blocks: [] };
-
-  componentDidMount = () => {
-    this.getInitialData();
-    WebSocketHandler.on('network', this.handleWebsocket);
-  };
-
-  componentWillUnmount = () => {
-    WebSocketHandler.removeListener('network', this.handleWebsocket);
-  };
+function DashboardTx() {
+  // transactions {Array} Array of transactions to show in the dashboard
+  const [transactions, setTransactions] = useState([]);
+  // blocks {Array} Array of blocks to show in the dashboard
+  const [blocks, setBlocks] = useState([]);
 
   /**
    * Get initial data to fill the screen and update the state with this data
    */
-  getInitialData = () => {
+  const getInitialData = useCallback(() => {
     txApi
       .getDashboardTx(DASHBOARD_BLOCKS_COUNT, DASHBOARD_TX_COUNT)
       .then(res => {
-        this.updateData(res.transactions, res.blocks);
+        updateData(res.transactions, res.blocks);
       })
       .catch(e => {
         // Error in request
         console.log(e);
       });
-  };
-
-  /**
-   * Handle websocket message to see if should update the list
-   */
-  handleWebsocket = wsData => {
-    if (wsData.type === 'network:new_tx_accepted') {
-      this.updateListWs(wsData);
-    }
-  };
+  }, []);
 
   /**
    * Update list because a new element arrived
    */
-  updateListWs = tx => {
+  const updateListWs = useCallback(tx => {
     if (tx.is_block) {
-      let blocks = this.state.blocks;
+      setBlocks(currentBlocks => {
+        // Create a new array to be mutated by the helper
+        const oldBlocks = [...currentBlocks];
+        const newBlocks = helpers.updateListWs(oldBlocks, tx, DASHBOARD_BLOCKS_COUNT);
 
-      blocks = helpers.updateListWs(blocks, tx, DASHBOARD_BLOCKS_COUNT);
-
-      // Finally we update the state again
-      this.setState({ blocks });
+        // Finally we update the state again
+        return newBlocks;
+      });
     } else {
-      let transactions = this.state.transactions;
+      setTransactions(currentTxs => {
+        // Create a new array to be mutated by the helper
+        const oldTransactions = [...currentTxs];
+        const newTransactions = helpers.updateListWs(oldTransactions, tx, DASHBOARD_TX_COUNT);
 
-      transactions = helpers.updateListWs(transactions, tx, DASHBOARD_TX_COUNT);
-
-      // Finally we update the state again
-      this.setState({ transactions });
+        // Finally we update the state again
+        return newTransactions;
+      });
     }
-  };
+  }, []);
+
+  /**
+   * Handle websocket message to see if should update the list
+   */
+  const handleWebsocket = useCallback(
+    wsData => {
+      if (wsData.type === 'network:new_tx_accepted') {
+        updateListWs(wsData);
+      }
+    },
+    [updateListWs]
+  );
+
+  useEffect(() => {
+    getInitialData();
+    WebSocketHandler.on('network', handleWebsocket);
+
+    return () => {
+      WebSocketHandler.removeListener('network', handleWebsocket);
+    };
+  }, [getInitialData, handleWebsocket]);
 
   /**
    * Update state data for transactions and blocks
    */
-  updateData = (transactions, blocks) => {
-    this.setState({ transactions, blocks });
+  const updateData = (transactionsData, blocksData) => {
+    setTransactions(transactionsData);
+    setBlocks(blocksData);
   };
 
-  /**
-   * Go to specific transaction or block page after clicking on the link
-   */
-  goToList = (e, to) => {
-    e.preventDefault();
-    this.props.history.push(to);
-  };
-
-  render() {
-    const renderTableBody = () => {
-      return (
-        <tbody>
-          {this.state.blocks.length ? (
-            <tr className="tr-title">
-              <td colSpan="2">
-                Blocks <a href="/blocks/">(See all blocks)</a>
-              </td>
-            </tr>
-          ) : null}
-          {renderRows(this.state.blocks)}
-          {this.state.transactions.length ? (
-            <tr className="tr-title">
-              <td colSpan="2">
-                Transactions <a href="/transactions/">(See all transactions)</a>
-              </td>
-            </tr>
-          ) : null}
-          {renderRows(this.state.transactions)}
-        </tbody>
-      );
-    };
-
-    const renderRows = elements => {
-      return elements.map((tx, idx) => {
-        return <TxRow key={tx.tx_id} tx={tx} />;
-      });
-    };
-
+  const renderTableBody = () => {
     return (
-      <div className="content-wrapper">
-        <div className="table-responsive">
-          <table className="table" id="tx-table">
-            <thead>
-              <tr>
-                <th className="d-none d-lg-table-cell">Hash</th>
-                <th className="d-none d-lg-table-cell">Timestamp</th>
-                <th className="d-table-cell d-lg-none" colSpan="2">
-                  Hash
-                  <br />
-                  Timestamp
-                </th>
-              </tr>
-            </thead>
-            {renderTableBody()}
-          </table>
-        </div>
-      </div>
+      <tbody>
+        {blocks.length ? (
+          <tr className="tr-title">
+            <td colSpan="2">
+              Blocks <a href="/blocks/">(See all blocks)</a>
+            </td>
+          </tr>
+        ) : null}
+        {renderRows(blocks)}
+        {transactions.length ? (
+          <tr className="tr-title">
+            <td colSpan="2">
+              Transactions <a href="/transactions/">(See all transactions)</a>
+            </td>
+          </tr>
+        ) : null}
+        {renderRows(transactions)}
+      </tbody>
     );
-  }
+  };
+
+  const renderRows = elements => {
+    return elements.map(tx => <TxRow key={tx.tx_id} tx={tx} />);
+  };
+
+  return (
+    <div className="content-wrapper">
+      <div className="table-responsive">
+        <table className="table" id="tx-table">
+          <thead>
+            <tr>
+              <th className="d-none d-lg-table-cell">Hash</th>
+              <th className="d-none d-lg-table-cell">Timestamp</th>
+              <th className="d-table-cell d-lg-none" colSpan="2">
+                Hash
+                <br />
+                Timestamp
+              </th>
+            </tr>
+          </thead>
+          {renderTableBody()}
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default DashboardTx;
