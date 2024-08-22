c1096ec90dd31b292a07b1c37f80aa519eca2d20
tuliomir <tulio.mir@gmail.com>
2024-08-22 12:31:16 -0300

    refactor: PushTx to functional component

--- src/screens/PushTx.js
+++ src/screens/PushTx.js
@@ -5,92 +5,76 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import TxTextInput from '../components/tx/TxTextInput';
 import txApi from '../api/txApi';
 
-class PushTx extends React.Component {
-  constructor(props) {
-    super(props);
+function PushTx() {
+  const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [canForce, setCanForce] = useState(false);
+  const [force, setForce] = useState(false);
+  const [dataToPush, setDataToPush] = useState('');
 
-    this.state = {
-      success: false,
-      errorMessage: null,
-      canForce: false,
-      force: false,
-      dataToPush: '',
-    };
+  const textRef = useRef();
 
-    this.buttonClicked = this.buttonClicked.bind(this);
-    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
-    this.handleChangeData = this.handleChangeData.bind(this);
+  async function buttonClicked() {
+    setSuccess(false);
+    const data = await txApi.pushTx(dataToPush, force);
+
+    setSuccess(!!data.success);
+    if (data.success) {
+      setErrorMessage(null);
+      setCanForce(false);
+      setForce(false);
+    } else {
+      setErrorMessage(data.message);
+      setCanForce(data.can_force);
+    }
   }
 
-  buttonClicked() {
-    this.setState({ success: false });
-    txApi.pushTx(this.state.dataToPush, this.state.force).then(
-      data => {
-        if (data.success) {
-          this.setState({ success: true, errorMessage: null, canForce: false, force: false });
-        } else {
-          this.setState({ success: false, errorMessage: data.message, canForce: data.can_force });
-        }
-      },
-      e => {
-        // Error in request
-        console.log(e);
-      }
-    );
+  function handleCheckboxChange(e) {
+    setForce(e.target.checked);
   }
 
-  handleCheckboxChange(e) {
-    this.setState({ force: e.target.checked });
+  function handleChangeData(e) {
+    setDataToPush(e.target.value);
   }
 
-  handleChangeData(e) {
-    this.setState({ dataToPush: e.target.value });
-  }
-
-  render() {
-    const renderForceCheckbox = () => {
-      return (
-        <div className="form-check checkbox-wrapper mb-3">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            id="force"
-            onChange={this.handleCheckboxChange}
-          />
-          <label className="form-check-label" htmlFor="force">
-            Force push
-          </label>
-        </div>
-      );
-    };
-
+  const renderForceCheckbox = () => {
     return (
-      <div className="content-wrapper">
-        <TxTextInput
-          ref={node => {
-            this.child = node;
-          }}
-          buttonClicked={this.buttonClicked}
-          action="Push tx"
-          onChange={this.handleChangeData}
-          otherAction="decode"
-          link="/decode-tx/"
-          helpText="Write your transaction in hex value and click the button to send it to the network. (We do not push blocks to the network, only transactions)"
+      <div className="form-check checkbox-wrapper mb-3">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="force"
+          onChange={handleCheckboxChange}
         />
-        {this.state.canForce ? renderForceCheckbox() : null}
-        {this.state.success ? (
-          <span className="text-success">Transaction pushed to the network with success!</span>
-        ) : null}
-        {this.state.errorMessage ? (
-          <span className="text-danger">{this.state.errorMessage}</span>
-        ) : null}
+        <label className="form-check-label" htmlFor="force">
+          Force push
+        </label>
       </div>
     );
-  }
+  };
+
+  return (
+    <div className="content-wrapper">
+      <TxTextInput
+        ref={textRef}
+        buttonClicked={buttonClicked}
+        action="Push tx"
+        onChange={handleChangeData}
+        otherAction="decode"
+        link="/decode-tx/"
+        helpText="Write your transaction in hex value and click the button to send it to the network. (We do not push blocks to the network, only transactions)"
+      />
+      {canForce ? renderForceCheckbox() : null}
+      {success ? (
+        <span className="text-success">Transaction pushed to the network with success!</span>
+      ) : null}
+      {errorMessage ? <span className="text-danger">{errorMessage}</span> : null}
+    </div>
+  );
 }
 
 export default PushTx;
