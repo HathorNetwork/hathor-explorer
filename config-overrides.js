121ba60d5671623db92ffcf8a958aa332e90e4ea
Tulio Miranda <tulio.mir@gmail.com>
2025-02-21 16:46:29 -0300

    feat: cra5 packages and polyfills

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    path: require.resolve('path-browserify'),
+    vm: require.resolve('vm-browserify'),
+  };
+  return config;
+};
