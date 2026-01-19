bd7d60e080bf4140d7f4b5f3c15b7f1a993a22c9
raul-oliveira <mrraul65@gmail.com>
2026-01-19 16:28:10 -0300

    feat: adds fee model to the token list table
    Delete .claude/settings.json

--- src/components/token/TokenRow.js
+++ src/components/token/TokenRow.js
@@ -29,6 +29,25 @@ function TokenRow({ token }) {
     return <div className="table-tokens-symbol">{children}</div>;
   };
 
+  /**
+   * Returns the fee model label based on token version
+   * version 0 = Native token (HTR)
+   * version 1 = Deposit-based
+   * version 2 = Fee-based
+   */
+  const getFeeModelLabel = version => {
+    switch (version) {
+      case 0:
+        return 'Native';
+      case 1:
+        return 'Deposit';
+      case 2:
+        return 'Fee';
+      default:
+        return '-';
+    }
+  };
+
   const renderNewUi = () =>
     isMobile ? (
       <tr onClick={_e => onRowClicked(token.uid)}>
@@ -47,6 +66,7 @@ function TokenRow({ token }) {
           <Symbol>{token.symbol}</Symbol>
         </td>
         <td className="d-lg-table-cell pe-3">{token.nft ? 'NFT' : 'Custom Token'}</td>
+        <td className="d-lg-table-cell pe-3">{getFeeModelLabel(token.version)}</td>
         <td className="d-lg-table-cell pe-3 date-cell">
           {dateFormatter.parseTimestampNewUi(token.transaction_timestamp)}
         </td>
@@ -62,6 +82,7 @@ function TokenRow({ token }) {
  * symbol: Token symbol
  * nft: If token is NFT or a Custom Token
  * transaction_timestamp: Timestamp of the transaction that created the token
+ * version: Token version (0=native, 1=deposit-based, 2=fee-based)
  */
 TokenRow.propTypes = {
   token: PropTypes.shape({
@@ -70,6 +91,7 @@ TokenRow.propTypes = {
     symbol: PropTypes.string.isRequired,
     nft: PropTypes.bool.isRequired,
     transaction_timestamp: PropTypes.number.isRequired,
+    version: PropTypes.number,
   }),
 };
 
